fc859ee028e1fd7a272e42b70a9582bb55f0d0c2
hfkemer <h.f.kemer@gmail.com>
2026-05-13 13:21:50 +0300

    format ve dosya yolları tamamen temizlendi

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.createRoot(root).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  )
+  );
 }
