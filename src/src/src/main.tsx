22be424d2370a0ea55f14ea84191105a504ca7b8
hfkemer <h.f.kemer@gmail.com>
2026-05-13 13:40:52 +0300

    Ultra-safe typescript conversion and build fix

--- src/src/src/main.tsx
+++ src/src/src/main.tsx
@@ -1,5 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root elementi bulunamadı! Lütfen index.html dosyasını kontrol edin.");
+}port ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
