a8612559d201ecbfccdbf713038032c99be527d4
hfkemer <h.f.kemer@gmail.com>
2026-05-13 13:33:36 +0300

    tum bozuk dosyalar silindi ve temizlendi

---
+++ src/src/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Site Başarıyla Çalıştı!</h1>
+      <p>Artık kodlarını buraya ekleyebilirsin.</p>
+    </div>
+  );
+}
+
+export default App;
